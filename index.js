d1486e4636d249b1037399ef73f46d47efa95839
Tom Dracz <tom.dracz@sanctus.io>
2022-01-26 20:13:41 +0000

    SWC test

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const swc = require('@swc/core');
+const buffer = fs.readFileSync('input.tsx');
+
+const swcOptions = {
+  jsc: {
+    parser: {
+      syntax: 'typescript',
+      tsx: true,
+    },
+  },
+};
+
+swc
+  .transform(buffer.toString(), {
+    filename: 'input.tsx',
+    ...swcOptions,
+  })
+  .then((output) => console.log(output.code));
